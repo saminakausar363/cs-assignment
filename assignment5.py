3ff114207003d9c834b436f752afea519b45d855
samina <saminarafique339@gmail.com>
2019-10-13 13:30:20 +0500

    My Second Commit

---
+++ assignment5.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("you must be the legendary god that comes from the sky")
+print("we have been waiting for you for a long time")
+
+
+x=6
+print(x)
+y=x*7
+print(y)
---
+++ assignment6.py
@@ -0,0 +1,22 @@
+name = input("samina")
+print ("hello",samina, "!")
+
+hours =int(input("6"))
+rate = float(input("2.5"))
+salary = hours*rate
+print("pay: {0: .2f}" .format(salary))
+
+width = 17
+height = 12.0
+print(width/2)
+print(type(width/2))
+print(width/2.0)
+print(type(width/2.0))
+print(height/3)
+print(type(height/3))
+print(1*2*3)
+print(type(1+2+3))
+
+celsius = float(input("32:"))
+fahrenheit = celsius*fraction(9 , 5)+ 32
+print("{0 : . 2f} c degree is {1: . 2f} f degree" .format(celsius, fahrenheit))
