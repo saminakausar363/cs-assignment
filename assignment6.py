cefdc225797d28e6d29c59e54301a14d720d6ad2
saminakausar363 <55921934+saminakausar363@users.noreply.github.com>
2019-10-15 08:06:44 +0500

    Update assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -1,22 +1,93 @@
-name = input("samina")
-print ("hello",samina, "!")
+print(4)
 
-hours =int(input("6"))
-rate = float(input("2.5"))
-salary = hours*rate
-print("pay: {0: .2f}" .format(salary))
+print(type("Hello World"))
+print(type(17))
+print(type(3.2))
+print(type("17"))
+print(type("3.2"))
 
-width = 17
-height = 12.0
+print(1,000,000)
+
+message="And now for something completely different"
+n=17
+pi=3.1415926
+print(message)
+print(n)
+print(pi)
+type(message)
+type(n)
+type(pi)
+
+print(1)
+x=2
+print(x)
+
+print(2*(3-1))
+
+print((1+1)**(5-2))
+
+print(2**1+1)
+
+print(3*1**3)
+
+print(2*3-1)
+
+print(6+4/2)
+
+print(5-3-1)
+
+quotient=7/3
+print(quotient)
+remainder=7%3
+print(remainder)
+
+first=10
+second=15
+print(first+second)
+
+first="100"
+second="150"
+print(first+second)
+
+first="test"
+second=3
+print(first*second)
+
+inp=input()
+print(inp)
+
+name=input("What is your name?\n")
+print(name)
+
+prompt="What is the airspeed velocity of unladen swallow?\n"
+speed=input(prompt)
+int(speed)
+
+
+hours=35.0
+rate=12.50
+pay=hours*rate
+print(pay)
+
+name=input("What is your name?\n")
+print("Hello",name,"!")
+
+hours=int(input("Enter hours:\n"))
+rate=float(input("Enter rate:\n"))
+salary=hours*rate
+print(salary)
+
+width=17
+height=12.0
 print(width/2)
 print(type(width/2))
 print(width/2.0)
 print(type(width/2.0))
 print(height/3)
 print(type(height/3))
-print(1*2*3)
-print(type(1+2+3))
+print(1+2*5)
+print(type(1+2*5))
 
-celsius = float(input("32:"))
-fahrenheit = celsius*fraction(9 , 5)+ 32
-print("{0 : . 2f} c degree is {1: . 2f} f degree" .format(celsius, fahrenheit))
+celsius=float(input("Enter degrees in celsius:\n"))
+fahrenheit=(celsius*(9/5))+32
+print(fahrenheit)
